ddad6f5c6ac69970d37c64a846203d8bac5d47d7
Hyuta217 <ee217055@meiji.ac.jp>
2023-10-08 04:52:58 +0900

    文字の強調

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+var path = require('path')
+
 var express = require("express")
 var app = express()
 var PORT = 3000
+
+app.use('/', express.static(path.resolve(__dirname, 'public')))
+
 app.get("/hello-world", (req, res) => {
 res.send("Hello,World!")
 })
